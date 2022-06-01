be586579ce6aad0154ec9149d330e1eea56dd1fe
Carlos A. B. Carucce <carloscarucce@gmail.com>
2022-06-01 10:42:51 -0300

    #1 - Add support to multibyte padding

--- mb_str_pad.php
+++ mb_str_pad.php
@@ -1,20 +1,47 @@
 <?php
 
-/**
- * mb_str_pad
- *
- * @param string $input
- * @param int $pad_length
- * @param string $pad_string
- * @param int $pad_type
- *
- * @return string
- *
- * @author Kari "Haprog" Sderholm
- * @see https://gist.github.com/nebiros/226350
- */
-function mb_str_pad($input, $pad_length, $pad_string = ' ', $pad_type = STR_PAD_RIGHT)
-{
-    $diff = strlen( $input ) - mb_strlen( $input );
-    return str_pad( $input, $pad_length + $diff, $pad_string, $pad_type );
+if (!function_exists('mb_str_pad')) {
+    /**
+     * Multibyte String Pad
+     *
+     * Functionally, the equivalent of the standard str_pad function, but is capable of successfully padding multibyte strings.
+     *
+     * @param string $input The string to be padded.
+     * @param int $length The length of the resultant padded string.
+     * @param string $padding The string to use as padding. Defaults to space.
+     * @param int $padType The type of padding. Defaults to STR_PAD_RIGHT.
+     * @param string|null $encoding The encoding to use, defaults to null.
+     *
+     * @see https://gist.github.com/rquadling/c9ff12755fc412a6f0d38f6ac0d24fb1
+     *
+     * @return string A padded multibyte string.
+     */
+    function mb_str_pad($input, $length, $padding = ' ', $padType = STR_PAD_RIGHT, $encoding = null)
+    {
+        $result = $input;
+        if (($paddingRequired = $length - mb_strlen($input, $encoding)) > 0) {
+            switch ($padType) {
+                case STR_PAD_LEFT:
+                    $result =
+                        mb_substr(str_repeat($padding, $paddingRequired), 0, $paddingRequired, $encoding).
+                        $input;
+                    break;
+                case STR_PAD_RIGHT:
+                    $result =
+                        $input.
+                        mb_substr(str_repeat($padding, $paddingRequired), 0, $paddingRequired, $encoding);
+                    break;
+                case STR_PAD_BOTH:
+                    $leftPaddingLength = floor($paddingRequired / 2);
+                    $rightPaddingLength = $paddingRequired - $leftPaddingLength;
+                    $result =
+                        mb_substr(str_repeat($padding, $leftPaddingLength), 0, $leftPaddingLength, $encoding).
+                        $input.
+                        mb_substr(str_repeat($padding, $rightPaddingLength), 0, $rightPaddingLength, $encoding);
+                    break;
+            }
+        }
+
+        return $result;
+    }
 }
